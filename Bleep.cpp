0d2db3f418c2ced7dcd5eea23c5290e18c848356
jessebarnes1 <89753098+jessebarnes1@users.noreply.github.com>
2022-12-24 16:03:07 -0800

    Create Bleep.cpp

---
+++ Bleep.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+
+int main()
+{
+  string word = "broccoli";
+  string tex = "I sometimes eat broccoli. There are three interesting things about broccoli. Number One. Nobody knows how to spell it. Number Two. No matter how long you boil it, it's always cold by the time it reaches your plate. Number Three. It's green. #broccoli";
+}
